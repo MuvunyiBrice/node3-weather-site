8d101e944a0f153c3914253ab914592a27311784
Brice M <movebrice@gmail.com>
2020-02-23 11:06:54 +0200

    remove console.log call

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,3 @@
-console.log('Client side JS');
-
-
-
-
 const weatherForm = document.querySelector('form');
 const searchValue = document.querySelector('input');
 const messageOne = document.querySelector('#message-one');
